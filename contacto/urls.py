cced343a1e5fef8204204ee36b8a42b3a993d29c
AlejoFiesco <alfiesparra@hotmail.com>
2023-10-24 06:29:07 -0500

    Ya hace delete

--- contacto/urls.py
+++ contacto/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('persona/', PersonasView.as_view(), name='personas-list'),
     path('persona/<int:numero_documento>/', PersonasView.as_view(), name='personas-list-detail'),
     path('telefono/', TelefonosView.as_view(), name='edit-telefonos'),
-    path('emails/', EmailsView.as_view(), name='edit-emails')
+    path('telefono/<int: id>/', TelefonosView.as_view(), name='delete-telefonos'),
+    path('emails/', EmailsView.as_view(), name='edit-emails'),
+    path('emails/<int: id>/', EmailsView.as_view(), name='delete-emails')
+
 ]
--- contacto/views.py
+++ contacto/views.py
@@ -20,24 +20,74 @@ class PersonasView(APIView):
                 'personas': serializer.data
             }
             return Response(responseData, status = status.HTTP_200_OK)
+        
+    def post(self, request):
+        personaData = request.data.get('persona', {})
+        telefonoData = request.data.get('telefono', {})
+        emailData = request.data.get('email', {})
+        personaSerializer = PersonaSerializer(data = personaData)
+        telefonoSerializer = TelefonoSerializer(data = telefonoData)
+        emailSerializer = EmailSerializer(data=emailData)
+
+        if personaSerializer.is_valid():
+            persona = personaSerializer.save()
+            if telefonoSerializer.is_valid():
+                telefonoSerializer.save(persona=persona)
+            if emailSerializer.is_valid():
+                emailSerializer.save(persona=persona)
+            return Response(personaSerializer.data, status=status.HTTP_200_OK)
+        return Response(personaSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, numero_documento=None):
+        if(numero_documento == None): return(Response('Este número de documento no existe', status=status.HTTP_400_BAD_REQUEST))        
+        
+        try:
+            persona = Personas.objects.get(numero_documento = numero_documento)
+            persona.delete()
+            return Response('Contacto eliminado', status=status.HTTP_204_NO_CONTENT)
+        except Personas.DoesNotExist:
+            return Response('Este contacto no existe', status=status.HTTP_404_NOT_FOUND)
+
+            
+
 
 class TelefonosView(APIView):
     def post(self, request):
-        print(request.data)
         serializer = TelefonoSerializer(data = request.data)
         if (serializer.is_valid()):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            
+
             print(serializer.errors)
         return Response('Se enviaron datos incorrectos', status=status.HTTP_400_BAD_REQUEST)    
+    
+    def delete(self, request, id=None):
+        if(id == None): return(Response('Este número de telefono no existe', status=status.HTTP_400_BAD_REQUEST))        
+        
+        try:
+            telefono = Telefono.objects.get(id = id)
+            telefono.delete()
+            return Response('Telefono eliminado', status=status.HTTP_204_NO_CONTENT)
+        except Telefono.DoesNotExist:
+            return Response('Este telefono no existe', status=status.HTTP_404_NOT_FOUND)
+
 class EmailsView(APIView):
     def post(self, request):
-        print(request.data)
         serializer = EmailSerializer(data = request.data)
         if (serializer.is_valid()):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response('Se enviaron datos incorrectos', status=status.HTTP_400_BAD_REQUEST)    
+    
+    def delete(self, request, id=None):
+        if(id == None): return(Response('Este número de telefono no existe', status=status.HTTP_400_BAD_REQUEST))        
+        
+        try:
+            telefono = Personas.objects.get(id = id)
+            telefono.delete()
+            return Response('Telefono eliminado', status=status.HTTP_204_NO_CONTENT)
+        except Personas.DoesNotExist:
+            return Response('Este telefono no existe', status=status.HTTP_404_NOT_FOUND)
+
     
